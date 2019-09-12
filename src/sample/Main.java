b6ec81d2d5965e0574bd0927cfb640928904977f
gus0108 <15030078@itcelaya.edu.mx>
2019-09-12 12:41:23 -0500

    Cambio de titulo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Ingeniería de Software: Openness");
+        primaryStage.setTitle("Ingeniería de Software: Openness");//deja ya tus mamadas
         primaryStage.setScene(new Scene(root, 527, 447));
         primaryStage.show();
     }
