0131827e42b616ed208e5acb131bfde55abb4650
esqutia <15030086@itcelaya.edu.mx>
2019-09-12 12:28:12 -0500

    Empezando a conocer

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Ingeniería de Software: Openness");
+        primaryStage.setScene(new Scene(root, 527, 447));
         primaryStage.show();
     }
 
