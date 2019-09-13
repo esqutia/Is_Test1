2feafd5978bcf02a7f33296515efd497ae135064
esqutia <15030086@itcelaya.edu.mx>
2019-09-13 13:30:38 -0500

    Warm 1, terminado

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,20 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 public class Controller {
+    @FXML
+    TextField cad_warm1;
+    @FXML
+    Label reWarm1;
 
 
+    public void last3(KeyEvent keyEvent) {
+        Warmup1 obj=new Warmup1();
+        String texto=obj.conversion(cad_warm1.getText());
+        reWarm1.setText(texto);
+    }
 }
