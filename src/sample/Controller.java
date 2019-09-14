60205fe145bb9162f58b763920b91af144d9c923
gerar <15030135@itcelaya.edu.mx>
2019-09-14 12:00:06 -0500

    se agrego la interfaz del warm up 2 asi como su respectivo metodo de solucion

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,14 +7,20 @@ import javafx.scene.input.KeyEvent;
 
 public class Controller {
     @FXML
-    TextField cad_warm1;
+    TextField cad_warm1,cad_warm2;
     @FXML
-    Label reWarm1;
+    Label reWarm1,reWarm2;
 
 
     public void last3(KeyEvent keyEvent) {
         Warmup1 obj=new Warmup1();
         String texto=obj.conversion(cad_warm1.getText());
         reWarm1.setText(texto);
+    }
+
+    public void yak(){
+        Warmup2 obj=new Warmup2();
+        String cadena=obj.stringYak(cad_warm2.getText());
+        reWarm2.setText(cadena);
     }
 }
